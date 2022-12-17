0059233b3835afa985fa82afd6668a50072a5ecf
Randy Eckman <randy.eckman@nasa.gov>
2022-12-17 11:34:56 -0600

    day 16 part 2 slow but works

--- day16/include/valves.h
+++ day16/include/valves.h
@@ -58,50 +58,104 @@ class Network {
     }
 
     int dynProgDualMaxCost() {
-      std::cout << "setting up\n";
+      int maxcost = 0;
+
+      std::cout << "setting up...";
       std::unordered_set<std::string> unopened;
+      for (auto &v: m_data) if (v.second.get()->rate) unopened.insert(v.first);
+
       std::unordered_map<std::string, std::unordered_map<std::string, int>> dist;
-      for (auto &v: m_data) {
-        if (v.second.get()->rate) unopened.insert(v.first);
-        for (auto &u: m_data) dist[u.first][v.first] = dijkstra(u.first, v.first);
-      }
-
-      std::unordered_map<std::string, int> cost;
-
-      // compute all the dual permutations first
-      std::unordered_set<std::string> startcombos;
+      std::unordered_map<std::string, int> decoder;
+      int i = 0;
       for (auto v: unopened) {
-        for (auto u: unopened) {
-          if (u == v) continue;
-          startcombos.insert((u < v)? u + v : v + u);
+        for (auto u: unopened) dist[u][v] = dijkstra(u, v);
+        dist[m_root][v] = dijkstra(m_root, v);
+        decoder[v] = i++;
+      }
+      std::cout << "complete\n";
+
+      // find all time-feasible chains first
+      std::cout << "building chains...";
+      std::unordered_map<std::string, uint64_t> visited;
+      buildChain(TIMELIMITPT2, "", m_root, decoder, unopened, visited, dist);
+      std::cout << "complete\n";
+
+      // compute cost for remaining chains
+      std::cout << "compute costs...";
+      std::unordered_map<std::string, int> cost;
+      computeChainCost(TIMELIMITPT2, visited, dist, cost);
+      std::cout << "complete\n";
+
+      // compute sums of non-overlapping, find max
+      std::cout << "max search...\n";
+      int tmpcost;
+      int j = 0, total = cost.size();
+      long searchsize = total*total, current = 0;
+      i = 0;
+      for (auto a: cost) {
+        i++;
+        for (auto b: cost) {
+          j++;
+          current++;
+          if (!(current%1000000)) std::cout << 100.0*current/searchsize << "%\n";
+          if (a.first == b.first) continue;
+          if (visited[a.first] & visited[b.first]) continue;
+          tmpcost = a.second + b.second;
+          if (tmpcost > maxcost)
+            maxcost = tmpcost;
         }
       }
-      int combocount = startcombos.size();
-      std::cout << "combocount: " << combocount << "\n";
-
-      std::string a, b, chain;
-      int dista, distb, tmpcost, maxcost = 0, i = 0;
-      for (auto pair: startcombos) {
-        a = pair.substr(0, 2);
-        b = pair.substr(2, 2);
-        dista = dist[m_root][a];
-        distb = dist[m_root][b];
-
-        if ((dista + 1) >= TIMELIMITPT2 || (distb + 1) >= TIMELIMITPT2) continue;
-        chain = (dista < distb)? 'a' + a + 'b' + b : 'b' + b + 'a' + a;
-        tmpcost = getCost(a, TIMELIMITPT2 - dista) + getCost(b, TIMELIMITPT2 - distb);
-        cost[chain] = tmpcost;
-        std::unordered_set<std::string> newunopened(unopened);
-        newunopened.erase(a);
-        newunopened.erase(b);
-        std::cout << "test: " << chain << " (" << 100.0*++i/combocount << "%)\n";
-        tmpcost = computeDualChain(maxcost, a, TIMELIMITPT2 - dista - 1, b, TIMELIMITPT2 - distb - 1,
-          chain, newunopened, cost, dist);
-        if (tmpcost > maxcost) maxcost = tmpcost;
-      }
+      std::cout << "complete\n";
 
       return maxcost;
     }
+
+    // NOTE: THIS TOOK TOO LONG AND TOO INEFFICIENT
+    // int dynProgDualMaxCost() {
+    //   std::cout << "setting up\n";
+    //   std::unordered_set<std::string> unopened;
+    //   std::unordered_map<std::string, std::unordered_map<std::string, int>> dist;
+    //   for (auto &v: m_data) {
+    //     if (v.second.get()->rate) unopened.insert(v.first);
+    //     for (auto &u: m_data) dist[u.first][v.first] = dijkstra(u.first, v.first);
+    //   }
+
+    //   std::unordered_map<std::string, int> cost;
+
+    //   // compute all the dual permutations first
+    //   std::unordered_set<std::string> startcombos;
+    //   for (auto v: unopened) {
+    //     for (auto u: unopened) {
+    //       if (u == v) continue;
+    //       startcombos.insert((u < v)? u + v : v + u);
+    //     }
+    //   }
+    //   int combocount = startcombos.size();
+    //   std::cout << "combocount: " << combocount << "\n";
+
+    //   std::string a, b, chain;
+    //   int dista, distb, tmpcost, maxcost = 0, i = 0;
+    //   for (auto pair: startcombos) {
+    //     a = pair.substr(0, 2);
+    //     b = pair.substr(2, 2);
+    //     dista = dist[m_root][a];
+    //     distb = dist[m_root][b];
+
+    //     if ((dista + 1) >= TIMELIMITPT2 || (distb + 1) >= TIMELIMITPT2) continue;
+    //     chain = (dista < distb)? 'a' + a + 'b' + b : 'b' + b + 'a' + a;
+    //     tmpcost = getCost(a, TIMELIMITPT2 - dista) + getCost(b, TIMELIMITPT2 - distb);
+    //     cost[chain] = tmpcost;
+    //     std::unordered_set<std::string> newunopened(unopened);
+    //     newunopened.erase(a);
+    //     newunopened.erase(b);
+    //     std::cout << "test: " << chain << " (" << 100.0*++i/combocount << "%)\n";
+    //     tmpcost = computeDualChain(maxcost, a, TIMELIMITPT2 - dista - 1, b, TIMELIMITPT2 - distb - 1,
+    //       chain, newunopened, cost, dist);
+    //     if (tmpcost > maxcost) maxcost = tmpcost;
+    //   }
+
+    //   return maxcost;
+    // }
 
     // int randyMaxCost() {
     //   std::unordered_set<std::string> unopened;
@@ -316,5 +370,48 @@ class Network {
         }
       }
       return maxcost;
+    }
+
+    void buildChain(int timeleft, std::string chain, std::string here,
+      std::unordered_map<std::string, int> &decoder,
+      std::unordered_set<std::string> unopened, std::unordered_map<std::string, uint64_t> &visited,
+      std::unordered_map<std::string, std::unordered_map<std::string, int>> &dist)
+    {
+      std::string newchain;
+      int traveltime, newtime;
+      uint64_t visit = (visited.count(chain))? visited[chain] : 0;
+      for (auto v: unopened) {
+        traveltime = dist[here][v];
+        newtime = timeleft - traveltime - 1;
+        if (newtime < 1) continue;
+        newchain = chain + v;
+        visited[newchain] = visit | (1 << decoder[v]);
+        std::unordered_set<std::string> newunopened(unopened);
+        newunopened.erase(v);
+        buildChain(newtime, newchain, v, decoder, newunopened, visited, dist);
+      }
+    }
+
+    void computeChainCost(int maxtime, std::unordered_map<std::string, uint64_t> &visited,
+      std::unordered_map<std::string, std::unordered_map<std::string, int>> &dist,
+      std::unordered_map<std::string, int> &cost)
+    {
+      int timeleft, traveltime, len, tmpcost;
+      std::string here, next;
+      for (auto visit: visited) {
+        tmpcost = 0;
+        timeleft = maxtime;
+        here = m_root;
+        len = visit.first.length();
+        for (int i = 0; i < len; i += 2) {
+          next = visit.first.substr(i, 2);
+          traveltime = dist[here][next];
+          timeleft -= traveltime;
+          tmpcost += getCost(next, timeleft);
+          timeleft--;
+          here = next;
+        }
+        cost[visit.first] = tmpcost;
+      }
     }
 };
